402865195f5cfbbe40df4dba4e28cd30f4a08817
Juan David Orejuela Bolanos <juan.orejuela@hotmail.com>
2016-03-19 03:31:26 -0500

    Navigation trought the map and select level Works

--- Assets/Scripts/MoveCharacterTroughtMap.cs
+++ Assets/Scripts/MoveCharacterTroughtMap.cs
@@ -3,8 +3,11 @@ using System.Collections;
 
 public class MoveCharacterTroughtMap : MonoBehaviour {
 
+	// List that contains all the points that represents the levels
 	public GameObject[] levels;
+	// Current level in which the character is stand
 	private int currentLevel = 0;
+	// The speed for move to level A to level B
 	private float speed = 90f;
 
 	void Start () {
@@ -12,26 +15,27 @@ public class MoveCharacterTroughtMap : MonoBehaviour {
 	}
 
 	void Update () {
-		//transform.position = Vector3.Lerp(transform.position, new Vector3(levels[currentLevel].transform.position.x * speed, levels[currentLevel].transform.position.y * speed, 0), 0),
 		float step = speed * Time.deltaTime;
 		if(levels.Length > 0){
 			transform.position = Vector3.MoveTowards(transform.position, levels[currentLevel].transform.position, step);
 		}
-		Debug.Log(currentLevel);
 	}
 
+	// This method works when you press "Next Level" button to pass to Level A to Level B
 	public void nextLevel(){
 		if(currentLevel < levels.Length - 1){
 			currentLevel ++;
 		}
 	}
 
+	// This method works when you press "Previous Level" button to pass to Level B to Level A
 	public void previousLevel(){
 		if(currentLevel > 0){
 			currentLevel --;
 		}
 	}
 
+	// When you choose a level to play, the level selected will be loaded in the next scene
 	public void selectLevel(){
 		PlayerPrefs.SetInt("levelSelected", currentLevel);
 		Application.LoadLevel("level"+currentLevel);
