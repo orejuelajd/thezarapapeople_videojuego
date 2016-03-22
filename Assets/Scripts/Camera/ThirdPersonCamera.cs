2a124344c1b212b7173dfa70f40a2e69b39f4c2f
christiada1 <c.david.granada@gmail.com>
2016-03-21 21:45:34 -0500

    Configuración de la CAMARA PRINCIPAL
    1. Se crearon dos carpetas, para organizar los scripts: una carpeta para
    la cámara y otra para el jugador.
    2. se amplió el terreno y se le puso un material
    3. se Creó el Script de la cámara, en dónde está siempre calcula por
    medio de 2 vectores, en donde se debe ubicar siempre que el personaje se
    mueva en cualquier dirección. y hace una interpolación para suavizar la
    acción de seguirlo, para que las transiciones no se vean bruscas.
    4. se ajustó la distancia del Canvas del botón, para que quedara entre
    la cámara y el jugador, para que así se pueda ver siempre el botón en la
    càmara.
    5. se le puso un Tag al character, que es Player. No tocarlo por favor,
    porque la cámara persigue a quién tiene este Tag.
    6. Borre un script que se llamaba Control_Default_Jugador

---
+++ Assets/Scripts/Camera/ThirdPersonCamera.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThirdPersonCamera : MonoBehaviour {
+	
+	//Region de Variables Privadas
+	
+	[SerializeField]
+	private float distanceAway;
+	[SerializeField]
+	private float distanceUp;
+	[SerializeField]
+	private float smooth;
+	[SerializeField]
+	private Transform followCharacter;
+	private Vector3 targetPosition;
+	
+	
+	// Use this for initialization
+	void Start () {
+		
+		
+		followCharacter = GameObject.FindWithTag("Player").transform;
+		
+		
+		
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	
+	void LateUpdate(){
+		
+		//Ajuste de la posicion del jugador, para que el desplazamiento  de la camara sea el correcto
+		
+		targetPosition = followCharacter.position + (followCharacter.up * distanceUp) - (followCharacter.forward * distanceAway);
+		Debug.DrawRay (followCharacter.position, Vector3.up * distanceUp, Color.red);
+		Debug.DrawRay (followCharacter.position,-1f*followCharacter.forward*distanceAway,Color.blue);
+		Debug.DrawLine(followCharacter.position, targetPosition, Color.magenta);
+		
+		
+		
+		//Haciendo una translacion suave entre la posición actual de la camara y la posición en la que estara
+		//Interpolacion entre los dos vectores de posicion del jugador
+		
+		transform.position = Vector3.Lerp (transform.position, targetPosition, Time.deltaTime*smooth);
+		
+		
+		//Nos aseguramos de que la camara esta mirando en el lado correcto, es decir mirando al personaje
+		
+		transform.LookAt (followCharacter);
+		
+		
+		
+		
+	}
+	
+}
