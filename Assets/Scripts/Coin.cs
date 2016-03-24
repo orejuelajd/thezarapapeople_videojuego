9df01ff48bb9032b35248772666ceb0f72d2c8fd
Juan David Orejuela Bolanos <juan.orejuela@hotmail.com>
2016-03-23 19:25:19 -0500

    Diamond logic works and Slow Fall adjusted

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Coin: MonoBehaviour {
+
+	// Game object that represents to Character/Player
+	private GameObject player;
+
+	void Start(){
+		player = GameObject.Find("player");
+	}
+
+	void Update(){
+	}
+
+	// Method that works when an object collides with
+	// the coin (this game object), if the collider is
+	// the character the coin dissapear
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Player") 
+		{
+			if(this.tag == "diamond"){
+				player.GetComponent<PlayerParams>().diamonds ++;
+			}
+			Destroy (this.gameObject);
+		}
+	}
+}
